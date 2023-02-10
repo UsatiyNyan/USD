1fdfb676e4cf153f2d19237c4cb25e651296dd76
UsatiyNyan <kirillarionov123456@gmail.com>
2023-02-10 12:40:21 +0100

    [TBB-MIGRATION] fix tests: missing libraries

--- pxr/base/work/testenv/testWorkLoops.cpp
+++ pxr/base/work/testenv/testWorkLoops.cpp
@@ -35,6 +35,7 @@
 #include <functional>
 
 #include <cstdio>
+#include <cstring>
 #include <numeric>
 #include <iostream>
 #include <vector>
@@ -167,7 +168,7 @@ _DoSignatureTest()
 int
 main(int argc, char **argv)
 {
-    const bool perfMode = ((argc > 1) && !strcmp(argv[1], "--perf")); 
+    const bool perfMode = ((argc > 1) && !std::strcmp(argv[1], "--perf"));
     const size_t arraySize = 1000000;
     const size_t numIterations = perfMode ? 1000 : 1;
 
